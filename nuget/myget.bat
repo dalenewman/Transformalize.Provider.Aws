895bdd32c547e9c7236908752c6a1311a84d5bb5
dalenewman <dalenewman@gmail.com>
2021-01-20 14:21:23 -0500

    Added Nuget

---
+++ nuget/myget.bat
@@ -0,0 +1,2 @@
+nuget push "c:\temp\modules\Transformalize.Transform.Aws.0.8.33-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Aws.Autofac.0.8.33-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
---
+++ nuget/nuget.bat
@@ -0,0 +1,2 @@
+nuget pack Transformalize.Transform.Aws.nuspec -OutputDirectory "c:\temp\modules"
+nuget pack Transformalize.Transform.Aws.Autofac.nuspec -OutputDirectory "c:\temp\modules"
